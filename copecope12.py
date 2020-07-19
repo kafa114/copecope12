68d77b564536b5f395acfed39c8ec4700b9638d1
kafa114 <68196133+kafa114@users.noreply.github.com>
2020-07-19 22:50:40 +0900

    Update copecope12.py

--- copecope12.py
+++ copecope12.py
@@ -3,12 +3,14 @@ import asyncio
 
 client = discord.Client()
 
-if message.content.startswith('/eval'):
-    try:
-        cmd = message.content[6:]
-        output = eval(cmd)
-        await message.channel.send(output)
-    except:
-        await message.channel.send("오류가 발생했습니다.")
+@client.event
+async def on_message(message):
+    if message.content.startswith('/eval'):
+        try:
+            cmd = message.content[6:]
+            output = eval(cmd)
+            await message.channel.send(output)
+        except:
+            await message.channel.send("오류가 발생했습니다.")
 
 client.run("NzMxODcyNjkxODMzMDEyMzA0.Xw6Vxg.Ag8dlbl4BRf9OOfU17g1BnnT2ns")
